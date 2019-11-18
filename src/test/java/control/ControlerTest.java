3120a83143b6cee834e68cb6539a5d24343bf41a
Jean-Blas IMBERT <jean-blas.imbert@amadeus.com>
2019-11-18 10:54:00 +0100

    Add mockito final class plugin

--- src/test/java/control/ControlerTest.java
+++ src/test/java/control/ControlerTest.java
@@ -8,6 +8,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
+//import static org.mockito.Mockito.mock;
+//import static org.mockito.Mockito.when;
 
 class ControlerTest {
 
